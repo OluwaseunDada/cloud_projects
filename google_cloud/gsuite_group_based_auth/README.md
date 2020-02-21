7b0ce15f242998f7115d2d2e630e0ccf66759e32
Alexander Dada <alexander.dada@helsinki.fi>
2020-02-21 11:10:47 +0200

    authentication users using the gsuite directory api

---
+++ google_cloud/gsuite_group_based_auth/README.md
@@ -0,0 +1,6 @@
+
+Directory API Client Library for Python:
+
+https://developers.google.com/admin-sdk/directory/v1/api-lib/python
+
+
---
+++ google_cloud/gsuite_group_based_auth/testgroup_based_auth.py
@@ -0,0 +1,8 @@
+
+
+from  group_based_auth  import verify_membership
+
+username = "someuser@email.com"
+r = verify_membership(username)
+print(r)
+
